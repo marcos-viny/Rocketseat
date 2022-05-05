78c89379fd22d9e06c9eb386f9ac0f36aae75daf
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-05-04 23:46:26 -0300

    ajuste diversos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,41 +1,24 @@
-import { useState } from 'react'
+interface ButtonProps{
+  text?: string;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <>
+     <button className="bg-red-400 text-red-600">{props.text ?? "Default"}</button>
+     {/* <button>{ `${props.text} default`}</button>  */}
+    {/* <button>{ `${props.text ? props.text : "Default"}`}</button> */}
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!marcosVinicius</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+   <>
+   <Button text="OK" />
+   <Button text="Enviar" />
+   <Button />
+   </>
   )
 }
 
