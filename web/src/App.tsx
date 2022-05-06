f9ba5f4cc56e2bd7bbb8aa5160ee3f19f05137ed
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-05-05 23:09:43 -0300

    teste de biblioteca

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import { Widget } from "./components/widget";
+import { Widget } from "./components/Widget";
+
 
 export function App(){
   return <Widget />
--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,10 @@
-import { ChatCenteredDots } from 'phosphor-react';
+import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget(){
     return(
             <div>
-                {/* <h1><ChatCenteredDots /></h1> */}
+                <h1><ChatTeardropDots weight="fill" size={100}/></h1>
+            <p>Marcsdb</p>
             <p>Marcsdb</p>
             </div>
     )
