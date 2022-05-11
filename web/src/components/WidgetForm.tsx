0bef4a8a5e70200a770aaa4669cf9677f60a803a
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-05-11 14:28:58 -0300

    adicionando funcionalidade para os botões

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugImageURL from '../assets/images/bug.svg'
@@ -5,34 +7,36 @@ import ideiaImageURL from "../assets/images/ideia.svg";
 import thoughtImageURL from "../assets/images/thought.svg";
 
 
-const feedbackTypes: Array<any> = [
-     {
-        name:"Bug",
-        title: 'Problema',
-        image: {
-            source: bugImageURL,
-            alt: 'Imagem de um inseto'
-        }
-    },
-    {
-        name:"IDEIA",
-        title: 'Ideia',
-        image: {
-            source: ideiaImageURL,
-            alt: 'Imagem de uma lâmpada'
-        }
-    },
-    {
-        name:"OTHER",
-        title: 'Other',
-        image: {
-            source: thoughtImageURL,
-            alt: 'Imagem de um balão de pensamento'
-        }
+const feedBackTypes = {
+  Bug: {
+    title: 'Problema',
+    image: {
+        source: bugImageURL,
+        alt: 'Imagem de um inseto'
     }
-];
+},
+IDEIA:{
+    title: 'Ideia',
+    image: {
+        source: ideiaImageURL,
+        alt: 'Imagem de uma lâmpada'
+    }
+},
+OTHER:{
+    title: 'Other',
+    image: {
+        source: thoughtImageURL,
+        alt: 'Imagem de um balão de pensamento'
+    }
+}
+};
+
+type FeedBackTypes = keyof typeof feedBackTypes;
 
 export function WidGetForm() {
+
+  const [ feedbackTypes, setFeedbackTypes ] = useState<FeedBackTypes | null>(null);
+
   return (
     <div
       className="
@@ -43,15 +47,21 @@ export function WidGetForm() {
         <CloseButton />
       </header>
 
-        <div className=" flex py-8 gap-2 w-full">
-            { feedbackTypes.map((value:any, index:number) => {
-                
+        <div className="flex py-8 gap-2 w-full">
+            { Object.entries(feedBackTypes).map(([key, value]) => {
                 return (
-                    <button key={index}>
-                        <img src={value.image.source} alt={value.image.alt} />
-                        <span>{value.title}</span>
-                    </button>
-                )
+                   <button key={key}
+                   onClick={() => setFeedbackTypes(key as FeedBackTypes)}
+                   type="button"
+                   className="
+                   bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2
+                    border-transparent transition-colors ease-linear duration-300 hover:border-brand-500
+                    hover:scale-110 focus:border-brand-500 focus:outline-none focus:scale-110" 
+                    >
+                       <img src={value.image.source} alt={value.image.alt} />
+                      <span>{value.title}</span>
+                   </button>
+               ) 
             })}
         </div>
 
@@ -66,4 +76,4 @@ export function WidGetForm() {
       </footer>
     </div>
   );
-}
+};
