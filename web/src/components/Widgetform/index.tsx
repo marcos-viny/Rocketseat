d78ac9321f3052513062d872a487fed9e9a7088d
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-06-07 20:17:46 -0300

    Finalizando o front do feedBack

--- web/src/components/Widgetform/index.tsx
+++ web/src/components/Widgetform/index.tsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 import { CloseButton } from "../CloseButton";
 import { FeedBackTypes } from "./steps/ConstFeedbackTypes";
 import { FeedBackContentStep } from "./steps/FeedbackContentStep";
+import { FeedBackSuccessStep } from "./steps/FeedbackSuccessStep";
 import { FeedBackTypeStep } from "./steps/FeedbackTypeStep";
 
 export function WidGetForm() {
   const [feedBackType, setFeedBackType] = useState<FeedBackTypes | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedBack(){
     setFeedBackType(null);
+    setFeedbackSent(false);
   };
 
   return (
@@ -19,12 +22,20 @@ export function WidGetForm() {
         bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col 
         items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-      {!feedBackType ? (
+     {feedbackSent ? (
+       <FeedBackSuccessStep onFeedbackRestartRequested={handleRestartFeedBack}/>
+     ):
+     (
+       <>
+        {!feedBackType ? (
         <FeedBackTypeStep onFeedbackTypeChange={setFeedBackType} />
       ) : (
         <FeedBackContentStep feedbackType={feedBackType}
-        onFeedbackRestartRequested={handleRestartFeedBack}/>
+        onFeedbackRestartRequested={handleRestartFeedBack}
+        onFeedbackSent={() => setFeedbackSent(true)}/>
       )}
+       </>
+     )}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}
