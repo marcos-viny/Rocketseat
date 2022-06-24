0f3d0ac6b594d9e40da433ca7009e8015a59b519
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-06-24 10:50:54 -0300

    ajuste no import login

--- web/src/components/Widgetform/ScreenshotButton.tsx
+++ web/src/components/Widgetform/ScreenshotButton.tsx
@@ -2,7 +2,8 @@ import html2canvas from "html2canvas";
 import { backgroundPosition } from "html2canvas/dist/types/css/property-descriptors/background-position";
 import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
-import { Loading } from "../loading";
+import { Loading } from "../Loading";
+
 
 interface ScreenshotButtonProps{
     onScreenshotTook: (screenshot: string | null) => void;
