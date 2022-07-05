67c5f71598520654ffeb9810f98151b73a36c7cb
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-07-05 15:59:38 -0300

    config do Prisma

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express();
+
+app.get('/users', (req, res) => {
+return res.send('Hello word!')
+})
+
+app.listen(3333, () =>{
+    console.log('HTTP server running!');
+    
+})
